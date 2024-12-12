2dac197142a070b3100b7705bd20fd7957a445e8
Diego Guzmán <oropezaeduardo442@gmail.com>
2024-12-11 23:24:32 -0600

    El primer commit de mi curso de JavaScript

---
+++ assets/js/app.js
@@ -0,0 +1,45 @@
+//* EL CLÁSICO HOLA MUNDO: console.log("hello world")
+
+//! ¡NO OLVIDAR COLOCAR EL ";" PUNTO Y COMA AL FINAL DE CADA LÍNEA DE CÓDIGO POR FAVOR!
+
+
+// let a = 10;
+// var b = 10;
+// const c = 10; //? La palabra reservada "const" hace referencia a una constante, es decir, SU VALOR NO CAMBIA
+
+//* Así reasignamos los valores de nuestras variables en JavaScript
+// a = 20
+// b = 30
+
+
+//* El siguiente código, imprimirá el valor de las variables por consola (console = consola)
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+//! Tener errores es NORMAL, no pensar que eres pésimo por tenerlos.
+
+
+
+
+//* Diferencia entre LET y VAR (También se pueden declarar variables sin palabras reservadas de por medio, aunque, es MALA práctica)
+l = 55;
+var t = 24 //! MALA PRÁCTICA
+let z = 13 //* BUENA PRÁCTICA
+// let x = t + z
+// console.log(x);
+
+//* Forma de declarar varias variables con una sola palabra reservada "LET":
+let a = 10,
+    b = 20,
+    c = 'Hello ',
+    d = 'World',
+    x = a + b;
+
+const greetings = c + d;
+
+//--------------------------------------------------------------------------------------------
+c = "Hello again"
+
+
+var myName = "Diego";
