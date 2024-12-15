118ab11d7d9972c8fd6440841124958ffa8ff298
Diego Guzmán <oropezaeduardo442@gmail.com>
2024-12-15 03:36:10 -0600

    primeros pasos en tipos de datos primitivos

---
+++ assets/js/primitivos.js
@@ -0,0 +1,16 @@
+let nombre = "Diego Guzmán";
+console.log(nombre);
+
+nombre = 'Sofía Guzmán';
+console.log(nombre);
+
+nombre = "Ingrid Oropeza";
+nombre = `Ingrid Oropeza`;
+
+
+
+let symbol1 = Symbol('a')
+let symbol2 = Symbol('a')
+
+console.log(typeof symbol1)
+console.log(symbol1 === symbol2)
